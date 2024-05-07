7fb8f54b3a8de7ada96378a206b2e59589d87da5
Israfil666 <113192535+Israfil666@users.noreply.github.com>
2024-05-06 22:45:02 -0700

    dasds

---
+++ backend/routes/userRouters.js
@@ -0,0 +1,25 @@
+import express from "express";
+const router = express.Router();
+import {
+  authUser,
+  registerUser,
+  logoutUser,
+  getUserProfile,
+  updateUserProfile,
+  getUsers,
+  getUserById,
+  deleteUser,
+  updateUser,
+} from "../controllers/userControllers.js";
+import { protect, admin } from "../middleware/authMiddleware.js";
+
+router.route("/").post(registerUser).get(protect, admin, getUsers);
+router.post("/logout", logoutUser);
+router.post("/auth", authUser);
+router
+  .route("/profile")
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile);
+router.route("/:id").delete(deleteUser).get(getUserById).put(updateUser);
+
+export default router;
